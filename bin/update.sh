528c87893135d9f10700d13a791dea58a2803683
Naohiro Aota <naota@elisp.net>
2014-06-27 18:37:57 +0900

    Check portage

--- bin/update.sh
+++ bin/update.sh
@@ -10,7 +10,14 @@ DIR=${DIR:-$(realpath "$(dirname $0)")}
 # Update gentoo and portage container
 ${DIR}/build.sh gentoo && \
 ${DIR}/build.sh portage || exit 1
-docker rm portage
-if [ -z "$(docker ps -a|grep portage)" ]; then
+
+RUNNING=$(docker ps -a | grep "${NAMESPACE}/portage" | awk '{print $2}')
+LATEST_TAG=$(docker images naota/portage | grep -v TAG | grep -v latest | \
+  awk '{print $2}'|head -n 1)
+LATEST="${NAMESPACE}/portage:${LATEST_TAG}"
+if [ -z "${RUNNING}" -o "${RUNNING}" != "${LATEST}" ]; then
+  if [ -n "${RUNNING}" ]; then
+    docker rm portage || exit 1
+  fi
   docker run -v /usr/portage --name portage ${NAMESPACE}/portage true
 fi
