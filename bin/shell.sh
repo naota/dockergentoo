e0e9864ae1f3eedf6a7d76e0854bff5340bfc7ce
Naohiro Aota <naota@gentoo.org>
2014-07-07 13:30:08 +0900

    Generate overlay tree only if ebuild is specified

--- bin/shell.sh
+++ bin/shell.sh
@@ -9,24 +9,24 @@ ebuild=$1
 NAMESPACE=${NAMESPACE:-$(whoami)}
 DIR=${DIR:-$(realpath "$(dirname $0)/..")}
 
-ebuilddir=$(realpath $(dirname ${ebuild}))
-portageroot=$(realpath ${ebuilddir}/../..)
-relativedir=${ebuilddir#${portageroot}/}
-tmptree=$(realpath $(dirname $0)/../tmp)/${RANDOM}
-
 function cleanup {
   rm -rf $tmptree
   exit
 }
 
-trap cleanup SIGHUP SIGINT SIGTERM
-mkdir -p ${tmptree}/profiles ${tmptree}/metadata || exit 1
-tar -C ${portageroot} -cf - ${relativedir} | tar -C ${tmptree} -xf -
-echo "dockergentoo" > ${tmptree}/profiles/repo_name
-echo "masters = gentoo" > ${tmptree}/metadata/layout.conf
-
 volumes="-v ${DIR}/build:/build -v ${DIR}/result:/result "
-test -n "${ebuild}" && volumes="${volumes} -v ${tmptree}:/overlay "
+if [ -n "${ebuild}" ]; then
+    ebuilddir=$(realpath $(dirname ${ebuild}))
+    portageroot=$(realpath ${ebuilddir}/../..)
+    relativedir=${ebuilddir#${portageroot}/}
+    tmptree=$(realpath $(dirname $0)/../tmp)/${RANDOM}
+    trap cleanup SIGHUP SIGINT SIGTERM
+    mkdir -p ${tmptree}/profiles ${tmptree}/metadata || exit 1
+    tar -C ${portageroot} -cf - ${relativedir} | tar -C ${tmptree} -xf -
+    echo "dockergentoo" > ${tmptree}/profiles/repo_name
+    echo "masters = gentoo" > ${tmptree}/metadata/layout.conf
+    volumes="${volumes} -v ${tmptree}:/overlay "
+fi
 mkdir -p ${DIR}/build ${DIR}/result
 docker run -i -t --rm \
   --volumes-from portage --volumes-from distfiles \
